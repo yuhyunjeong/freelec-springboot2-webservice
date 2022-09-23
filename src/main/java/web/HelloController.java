08eff556e79021e0af27cbf7545629626d08eebc
jade <jadejung95@gmail.com>
2022-09-23 11:11:42 +0900

    HelloController 생성

---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // JSON을 반환하는 컨트롤러로 만들어줌. @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 해준다
+public class HelloController {
+
+    @GetMapping("/hello") // Http Method인 Get의 요청을 받을 수 있는 API를 만들어줌
+    public String hello(){
+        return "hello";
+    }
+}
