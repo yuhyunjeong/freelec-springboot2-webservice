25996b9e5755bce2d89dd7e2e12f0fd6ddda4c01
jade <jadejung95@gmail.com>
2022-09-29 23:20:55 +0900

    HelloControllerTest 테스트 코드

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,4 +1,4 @@
-package web;
+package com.jojoldu.book.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // JSON을 반환하는 컨트롤러로 만들어줌. @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 해준다
 public class HelloController {
 
-    @GetMapping("/hello") // Http Method인 Get의 요청을 받을 수 있는 API를 만들어줌
+    @GetMapping("/hello") // Http Method인 Get의 요청을 받을 수 있는 API를 만들어줌. 이 프로젝트는 /hello 로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 되었다
     public String hello(){
         return "hello";
     }
