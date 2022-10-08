7e862c12bc86f0ba59cd6b3d3064f457b7aad3cf
jade <jadejung95@gmail.com>
2022-10-08 22:54:34 +0900

    JPA 테스트 코드

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 별다른 설정이 없을 경우 H2 데이터베이스를 자동으로 실행.
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정.
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // 테이블 posts에 insert/update 쿼리를 실행. id 값이 있다면 update가, 없다면 insert 쿼리가 실행.
+                .title(title)
+                .content(content)
+                .author("jadejung95@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
